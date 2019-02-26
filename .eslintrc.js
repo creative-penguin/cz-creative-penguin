c81e24df8ed68697fda58e2837f2930ca7437007
Jim Jenkins <jimjenkins5@gmail.com>
2019-02-26 09:35:23 -0500

    chore(standards): add ESLint, fix linting issues, match our coding standards

---
+++ .eslintrc.js
@@ -0,0 +1,8 @@
+module.exports = {
+   extends: [ '@silvermine/eslint-config/node' ],
+
+   rules: {
+      'no-process-env': 'off',
+      'no-console': 'off',
+   },
+};
--- index.js
+++ index.js
@@ -1,13 +1,14 @@
-"format cjs";
+'use strict';
 
 var engine = require('./engine');
+
 var conventionalCommitTypes = require('conventional-commit-types');
 
 module.exports = engine({
-  types: conventionalCommitTypes.types,
-  defaultType: process.env.CZ_TYPE,
-  defaultScope: process.env.CZ_SCOPE,
-  defaultSubject: process.env.CZ_SUBJECT,
-  defaultBody: process.env.CZ_BODY,
-  defaultIssues: process.env.CZ_ISSUES
+   types: conventionalCommitTypes.types,
+   defaultType: process.env.CZ_TYPE,
+   defaultScope: process.env.CZ_SCOPE,
+   defaultSubject: process.env.CZ_SUBJECT,
+   defaultBody: process.env.CZ_BODY,
+   defaultIssues: process.env.CZ_ISSUES,
 });
